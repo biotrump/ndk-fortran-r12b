688a72eddd73bf5ebe8cb7a58e5e5deebe753363
Thomas Tsai <thomas@life100.cc>
2016-08-14 18:11:22 +0800

    NDK r12b

--- dl.sh
+++ dl.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #https://github.com/biotrump/android-gfortran
 #gcc 4.8 only, 4.9 can't compile NDK successfuly!
+#sudo aptitude -y install gcc-4.8 g++-4.8 gfortran-4.8
 sudo update-alternatives --display gcc
 #change the default gcc
 sudo update-alternatives --config gcc
 
 #1. cd to ~/NDK
-#download NDK toolchain r12b :https://developer.android.com/ndk/downloads/index.html
+#download NDK toolchain r12b from https://developer.android.com/ndk/downloads/index.html
 #unzip NDK toolchain
-#rename the r12b to "ndk" for later build fortran toolchain
-#2. mkdir toolchain under NDK
-#cd toolchain
+#rename the "android-ndk-r12b" to "ndk" for later build fortran toolchain
+#2. mkdir toolchain under ~/NDK
+#cd ~/NDK/toolchain
 #3. clone the following repos by manifest
 repo init -u https://android.googlesource.com/toolchain/manifest/
 cp manifest.xml .repo/manifest.xml
@@ -28,9 +29,11 @@ repo sync
 #git clone -b ndk-r12b https://android.googlesource.com/toolchain/sed
 #git clone -b ndk-r12b https://android.googlesource.com/toolchain/binutils
 
+#4. patch build_support.py
 #pushd ~/NDK/ndk/build/lib/
-#patch -p1  << build-lib-build_support.diff
+#patch build_support.py -p1  << build-lib-build_support.py.diff
 #popd
+#5. patch gcc to build fortran : "non StandAlone toolchain is used!
 #pushd gcc
 #patch -p1 << ../gcc.diff
 #./build.py
@@ -42,3 +45,4 @@ repo sync
 #you can run build.py under toolchain/gcc
 #cd gcc
 #./build.py
+
